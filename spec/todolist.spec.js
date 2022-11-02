a108decc87249b9f1d2d72a49cc0de98f458789e
patrik <patrik.ellini@outlook.com>
2022-11-02 14:48:51 +0000

    completed the core criteria

--- spec/todolist.spec.js
+++ spec/todolist.spec.js
@@ -1,1 +1,122 @@
+const {
+  setToCompleted,
+  getAllTodos,
+  getIncompleted,
+  getCompleted,
+  searchTodo,
+  deleteTodo
+} = require('../src/todolist')
 
+describe('getAllTodos', () => {
+  it('should print all the todos if any', () => {
+    const todoList = []
+    const result = getAllTodos(todoList)
+    expect(result).toEqual(false)
+  })
+})
+
+describe('setToCompleted', () => {
+  it('should change the todo status to completed', () => {
+    const todoList = [
+      { id: 1, description: 'go to the grocery', status: 'incomplete' },
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ]
+    const result = setToCompleted(todoList, 1)
+    expect(result).toEqual([
+      { id: 1, description: 'go to the grocery', status: 'complete' },
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ])
+  })
+  it('should change only the given id', () => {
+    const todoList = [
+      { id: 1, description: 'go to the grocery', status: 'incomplete' },
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ]
+    const result = setToCompleted(todoList, 1)
+    expect(result).not.toEqual([
+      { id: 1, description: 'go to the grocery', status: 'incompleted' },
+      { id: 2, description: 'clean the house', status: 'complete' }
+    ])
+  })
+})
+
+describe('getIncompleted', () => {
+  it('should return incompleted todos', () => {
+    const todoList = [
+      { id: 1, description: 'go to the grocery', status: 'complete' },
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ]
+    const result = getIncompleted(todoList)
+    expect(result).toEqual([
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ])
+  })
+
+  it('shold not return completed todos', () => {
+    const todoList = [
+      { id: 1, description: 'go to the grocery', status: 'complete' },
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ]
+    const result = getIncompleted(todoList)
+    expect(result).not.toEqual([
+      { id: 1, description: 'go to the grocery', status: 'complete' }
+    ])
+  })
+})
+
+describe('getCompleted', () => {
+  it('should return completed todos', () => {
+    const todoList = [
+      { id: 1, description: 'go to the grocery', status: 'complete' },
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ]
+    const result = getCompleted(todoList)
+    expect(result).toEqual([
+      { id: 1, description: 'go to the grocery', status: 'complete' }
+    ])
+  })
+  it('shold not return incompleted todos', () => {
+    const todoList = [
+      { id: 1, description: 'go to the grocery', status: 'complete' },
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ]
+    const result = getCompleted(todoList)
+    expect(result).not.toEqual([
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ])
+  })
+})
+
+describe('searchTodo', () => {
+  it('should return only the selected todo', () => {
+    const todoList = [
+      { id: 1, description: 'go to the grocery', status: 'complete' },
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ]
+    const result = searchTodo(todoList, 1)
+    expect(result).toEqual([
+      { id: 1, description: 'go to the grocery', status: 'complete' }
+    ])
+  })
+  it("should return a message if doesn't exist", () => {
+    const todoList = [
+      { id: 1, description: 'go to the grocery', status: 'complete' },
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ]
+    const result = searchTodo(todoList, 42)
+    expect(result).toBe('todos not found')
+  })
+})
+
+describe('deleteTodo', () => {
+  it('should delete the only the selected todo', () => {
+    const todoList = [
+      { id: 1, description: 'go to the grocery', status: 'incomplete' },
+      { id: 2, description: 'clean the house', status: 'incomplete' }
+    ]
+    const result = deleteTodo(todoList, 1)
+    expect(result).not.toEqual([
+      { id: 1, description: 'go to the grocery', status: 'incomplete' }
+    ])
+  })
+})
--- src/todolist.js
+++ src/todolist.js
@@ -1,1 +1,72 @@
+/* eslint-disable array-callback-return */
+const todoList = [
+  { id: 1, description: 'go to the grocery', status: 'complete' },
+  { id: 2, description: 'clean the house', status: 'incomplete' }
+]
 
+function getAllTodos(list) {
+  if (list.length) {
+    // console.log(list)
+    return
+  }
+  return false
+}
+
+function setToCompleted(list, id) {
+  list.forEach((todo) => {
+    if (todo.id === id) {
+      todo.status = 'complete'
+    }
+  })
+  return list
+}
+function getIncompleted(todoList) {
+  const incompleteTodos = []
+  todoList.forEach((todo) => {
+    if (todo.status === 'incomplete') {
+      incompleteTodos.push(todo)
+    }
+  })
+  return incompleteTodos
+}
+
+function getCompleted(todoList) {
+  const completeTodos = []
+  todoList.forEach((todo) => {
+    if (todo.status === 'complete') {
+      completeTodos.push(todo)
+    }
+  })
+  return completeTodos
+}
+
+function searchTodo(list, id) {
+  const selectedTodo = []
+  list.find((todo) => {
+    if (todo.id === id) {
+      selectedTodo.push(todo)
+    }
+  })
+  console.log(selectedTodo)
+  return selectedTodo.length ? selectedTodo : 'todos not found'
+}
+
+function deleteTodo(list, id) {
+  list.map((todo) => {
+    if (todo.id === id) {
+      list.splice(list.indexOf(todo), 1)
+    }
+  })
+  console.log(list)
+}
+
+deleteTodo(todoList, 1)
+
+module.exports = {
+  setToCompleted,
+  getAllTodos,
+  getIncompleted,
+  getCompleted,
+  searchTodo,
+  deleteTodo
+}
